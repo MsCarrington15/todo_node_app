1b8b0d374ace31f641d395fdaa2141cc63e10435
benedicta <addicobenedicta@gamil.com>
2021-12-21 13:18:08 +0000

    todo_app_v7

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,24 +1,41 @@
 const todo = require('../models/todo');
 
-function insertTodo(){
+function insertTodo(req, res){ 
+    console.log(req.body)
+    todo.create(req.body)
+   
+   .then(function(data){
+       res.status(200).json({success:true, message:'Data has been created',data:data});
+    })
+    .catch(function(error){
+        res.status(401).json('Not created' +error.message);
+    });
 
 }
 
-function updateTodoById(){
+function updateTodoById(req, res){
 
 
 }
 
-function deleteTodoById(){
+function deleteTodoById(req, res){
 
 }
 
-function getTodoById(){
+function getTodoById(req, res){
 
 }
 
 function getAllTodos(req, res){
-
+      todo.find({})
+      .then(function(data){
+          res.status(200).json({
+              success: true, data})
+      })
+      .catch(function(err){
+          res.status(404).json({
+              success: false, error: 'Cant get data: '+err.message})
+      })
 }
 module.exports = {
     insertTodo, updateTodoById, deleteTodoById, getTodoById, getAllTodos
--- express_app.js
+++ express_app.js
@@ -1,22 +1,24 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require('express');   //express is our server
+const mongoose = require('mongoose');    // helps server connects to database thru url path
 const todoController = require('./controllers/todoController');
 const server = express();
-const mongo_db_url = 'mongodb+srv://ms_naa:sprinkles93@cluster0.vegxf.mongodb.net/todos_db?retryWrites=true&w=majority'
+// const mongo_db_url = 'mongodb+srv://ms_naa:sprinkles93@cluster0.vegxf.mongodb.net/todos_db?retryWrites=true&w=majority'
+           const mongodb_db_url="mongodb://localhost/todos_db"                    // url helps connect database
 
+server.use(express.json());
 
-
-
-server.get('/todos', todoController.getAllTodos);
-server.post('/todos',todoController.insertTodo);
-server.put('/todos',  todoController.updateTodoById);
-server.delete('/todos', todoController.deleteTodoById);
 
 server.listen(4000, function(){
     console.log('Server has started to run in express');
-    mongoose.connect(mongo_db_url)
+    mongoose.connect(mongodb_db_url)
     .then(function(){
         console.log('DB is connected');
+        server.get('/todos', todoController.getAllTodos);
+        server.post('/todos',todoController.insertTodo);
+        server.get('/todos/:id',todoController.getTodoById);
+        server.put('/todos/:id',  todoController.updateTodoById);
+        server.delete('/todos/:id', todoController.deleteTodoById);
+
     })
     
     .catch(function(error){
