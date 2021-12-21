6820f69466a0e93b90d14492ed4838fda4c4f4b1
benedicta <addicobenedicta@gamil.com>
2021-12-21 14:16:19 +0000

    todo_app_express_v2

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -14,17 +14,47 @@ function insertTodo(req, res){
 }
 
 function updateTodoById(req, res){
-
+    const { id } = req.params;
+    const {isCompleted}=req.body;
+    todo.findByIdAndUpdate(id,{isCompleted:isCompleted})
+    .then(function(){
+        res.status(200).json({success: true, message: 'its succefully updated'});
+    })
+    .catch(function(err){
+        console.log('Cant delete todo: ', err.message);
+        res.status(404).json({success: false, message: 'Cant update try again. Try again'})
+    })
 
 }
 
 function deleteTodoById(req, res){
-
+    const { id } = req.params;
+    todo.findByIdAndDelete(id)
+    .then(function(){
+        res.status(200).json({success: true, message: 'Todo is deleted successfully'});
+    })
+    .catch(function(err){
+        console.log('Cant delete todo: ', err.message);
+        res.status(404).json({success: false, message: 'Todo cant delete. Try again'})
+    })
 }
 
 function getTodoById(req, res){
-
+    const {id}=req.params;
+    todo.findById(id)
+  
+    .then(function(data){
+        res.status(200).json({
+            success: true, data});
+    })
+    .catch(function(err){
+        res.status(404).json({
+            success: false, error: 'Cant get data: '+err.message});
+    });
+   
 }
+
+
 
 function getAllTodos(req, res){
       todo.find({})
--- express_app.js
+++ express_app.js
@@ -1,16 +1,17 @@
+require('dotenv').config();
 const express = require('express');   //express is our server
 const mongoose = require('mongoose');    // helps server connects to database thru url path
 const todoController = require('./controllers/todoController');
 const server = express();
-// const mongo_db_url = 'mongodb+srv://ms_naa:sprinkles93@cluster0.vegxf.mongodb.net/todos_db?retryWrites=true&w=majority'
-           const mongodb_db_url="mongodb://localhost/todos_db"                    // url helps connect database
+//const mongo_db_url = ''
+        //   const mongodb_db_url="mongodb://localhost/todos_db"                    // url helps connect database
 
 server.use(express.json());
 
 
 server.listen(4000, function(){
     console.log('Server has started to run in express');
-    mongoose.connect(mongodb_db_url)
+    mongoose.connect(process.env.ATLAS_URL)
     .then(function(){
         console.log('DB is connected');
         server.get('/todos', todoController.getAllTodos);
